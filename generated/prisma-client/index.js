e0566e70acfac6628dbb468589a6aa90e5fd3ab6
MichaelNey <ney.quentin.michael@gmail.com>
2019-03-21 16:01:58 -0700

    Created seedPrisma.js which creates 500 fake users to the database when you run 'prisma seed' to whatever prisma server you are seeding to. Updated datamodel.prisma to update our Type relations.

--- generated/prisma-client/index.js
+++ generated/prisma-client/index.js
@@ -5,13 +5,41 @@ var typeDefs = require("./prisma-schema").typeDefs;
 
 var models = [
   {
+    name: "Schedule",
+    embedded: false
+  },
+  {
+    name: "Workout",
+    embedded: false
+  },
+  {
+    name: "SavedWorkout",
+    embedded: false
+  },
+  {
+    name: "Exercise",
+    embedded: false
+  },
+  {
     name: "User",
+    embedded: false
+  },
+  {
+    name: "Note",
+    embedded: false
+  },
+  {
+    name: "BodyMetric",
+    embedded: false
+  },
+  {
+    name: "BodyMeasurement",
     embedded: false
   }
 ];
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: process.env.PRISMA_ENDPOINT
+  endpoint: `${process.env["PRISMA_ENDPOINT"]}`
 });
 exports.prisma = new exports.Prisma();
