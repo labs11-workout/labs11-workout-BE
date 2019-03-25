0de5d1bde330555cb4e3e321cc26f374a3dac1e9
MichaelNey <ney.quentin.michael@gmail.com>
2019-03-24 21:02:58 -0700

    Renamed userResolvers to userQueries, as the file will be specific to user related queries, while a new file called userMutations can be created for the Mutation object.

--- resolvers/index.js
+++ resolvers/index.js
@@ -1,10 +1,10 @@
-const userResolvers = require("./userResolvers");
+const userQueries = require("./userQueries");
 const typeResolvers = require("./typeResolvers");
 
 const resolvers = {
 	Query: {
 		info: () => `This is the API of CleanLift`,
-		...userResolvers
+		...userQueries
 	},
 	// Mutation: {}, Uncomment and add mutation resolvers inside.
 	...typeResolvers
