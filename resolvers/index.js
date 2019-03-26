bf724a2798be1cd991d996c44362221daabada65
MichaelNey <ney.quentin.michael@gmail.com>
2019-03-26 15:19:18 -0700

    Created getSchedules query.

--- resolvers/index.js
+++ resolvers/index.js
@@ -1,10 +1,13 @@
 const userQueries = require("./userQueries");
+const scheduleQueries = require("./scheduleQueries");
+
 const typeResolvers = require("./typeResolvers");
 
 const resolvers = {
 	Query: {
 		info: () => `This is the API of CleanLift`,
-		...userQueries
+		...userQueries,
+		...scheduleQueries
 	},
 	// Mutation: {}, Uncomment and add mutation resolvers inside.
 	...typeResolvers
