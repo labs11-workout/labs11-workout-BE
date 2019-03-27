7cf16cb76256ac218740693a9cb5b0bfef4ef1d8
Alexandra <alexswartz9753@gmail.com>
2019-03-27 13:20:32 -0400

    created edit and delete mutations for BodyMeasurement

--- resolvers/bodyMeasurement/bodyMeasurementMutations.js
+++ resolvers/bodyMeasurement/bodyMeasurementMutations.js
@@ -11,7 +11,20 @@ const addBodyMeasurement = async(root, args, context, info)=>
 const deleteBodyMeasurement = async(root, args, context, info)=>
     await context.prisma.deleteBodyMeasurement({id: args.id});
 
+const editBodyMeasurement = async(root, args, context, info)=>
+    await context.prisma.updateBodyMeasurement({
+        data: {
+            hips: args.hips,
+            waist: args.waist,
+            rightArm: args.rightArm,
+            leftArm: args.leftArm,
+            leftLeg: args.leftLeg,
+        },
+        where: {id: args.id}
+    });
+
 module.exports = {
     addBodyMeasurement,
-    deleteBodyMeasurement
+    deleteBodyMeasurement,
+    editBodyMeasurement
 }
