331da9db38fa2153ed4d254b85fe258e0922c1ae
MichaelNey <ney.quentin.michael@gmail.com>
2019-03-27 16:48:58 -0700

    Created editNote mutation which takes required arguments 'noteId' that is the ID of the note you want to edit, and a 'note' argument which is the new note you want to set it as.

--- resolvers/note/noteMutations.js
+++ resolvers/note/noteMutations.js
@@ -27,6 +27,13 @@ const addNote = async (root, args, context, info) => {
 	}
 };
 
+const editNote = async (root, args, context, info) =>
+	context.prisma.updateNote({
+		data: { note: args.note },
+		where: { id: args.noteId }
+	});
+
 module.exports = {
-	addNote
+	addNote,
+	editNote
 };
