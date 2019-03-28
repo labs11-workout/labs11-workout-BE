e9389bafa4ed374c25216bf2d6d04260a373bcb7
Alexandra <alexswartz9753@gmail.com>
2019-03-28 12:17:09 -0400

    completed mutation to editSavedWorkout for a previously created saved workout

--- resolvers/savedWorkout/savedWorkoutMutations.js
+++ resolvers/savedWorkout/savedWorkoutMutations.js
@@ -4,6 +4,13 @@ const addSavedWorkout = async (root, args, context, info)=>
         user: {connect: {authId: args.userId}}
     });
 
+const editSavedWorkout = async (root, args, context, info)=>
+    context.prisma.updateSavedWorkout({
+        data: {name: args.name},
+        where: {id: args.id}
+    });
+
 module.exports =  {
     addSavedWorkout,
+    editSavedWorkout
 }
