5b12b0bbc2ed0709c9853324152071ae621fd804
tulf <timzulf@gmail.com>
2019-03-29 13:41:36 -0400

    stripe backend

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
-
+const bodyParser = require('body-parser')
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const { GraphQLServer } = require("graphql-yoga");
 const { prisma } = require("./generated/prisma-client");
 const resolvers = require("./resolvers");
 const jwt = require("express-jwt");
 const jwksRsa = require("jwks-rsa");
+
 
 //This function checks to see if the token provided in Authorization header is valid according to our Auth0 service.
 const checkJwt = jwt({
@@ -43,6 +45,32 @@ const server = new GraphQLServer({
 });
 
 server.use("/", checkJwt, errorResponse);
+server.post('/payment', bodyParser.json(), checkJwt, async (req, res) => {
+	try{
+		console.log(req.body);
+		//console.log(req.request.body);
+		const token = req.body.stripeToken
+	const charge = await stripe.charges.create({
+		amount: 999,
+		currency: 'usd',
+		description: 'Clean Lift Premium Subsription',
+		source: token,
+		capture: true,
+	  });
+	  await prisma.updateUser({where: {
+		  authId: req.user.sub
+	  }, 
+	  data: {
+		  premium: true
+	  }})
+	  res.json("Payment Suceeded");
+	}
+	catch(err){
+		console.log(err);
+	}
+	//unique authID:
+	//req.user.sub
+});
 
 const port = process.env.PORT || 4000;
 
