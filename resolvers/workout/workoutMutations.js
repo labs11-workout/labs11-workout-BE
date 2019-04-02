1ab6346167e4cd763e649f6aff270ddd208c0638
MichaelNey <ney.quentin.michael@gmail.com>
2019-04-02 15:03:46 -0700

    Created Workouts Mutation to create a Workout for a schedule, based off of a SavedWorkout. Takes ScheduleId and SavedWorkoutId as arguments.

--- resolvers/workout/workoutMutations.js
+++ resolvers/workout/workoutMutations.js
@@ -1,22 +1,69 @@
-const addWorkout = async(root, args, context, info) => 
-    await context.prisma.createWorkout({
-        name: args.name,
-        schedule: {connect: {id: args.scheduleId}}
-    });
+const addWorkout = async (root, args, context, info) =>
+	await context.prisma.createWorkout({
+		name: args.name,
+		schedule: { connect: { id: args.scheduleId } }
+	});
 
+//Mutation to take a ScheduleId to add a Workout to, and a SavedWorkoutId for a SavedWorkout the new Workout will be based off of.
+const addWorkoutFromSavedWorkout = async (root, args, context, info) => {
+	try {
+		//Get Saved Workout
+		const foundSavedWorkout = await context.prisma.savedWorkout({
+			id: args.savedWorkoutId
+		});
+		//Get the Saved Workout's Exercises.
+		const foundSavedWorkoutExercises = await context.prisma
+			.savedWorkout({
+				id: args.savedWorkoutId
+			})
+			.exercises();
+		//Create a Workout based on the foundSavedWorkout
+		const newWorkout = await context.prisma.createWorkout({
+			name: foundSavedWorkout.name,
+			schedule: { connect: { id: args.scheduleId } }
+		});
+		//For every exercise in foundSavedWorkout, create a new exercise under the new workout.
+		for (let i = 0; i < foundSavedWorkoutExercises.length; i++) {
+			const {
+				name,
+				sets,
+				reps,
+				intervals,
+				duration,
+				intensity
+			} = foundSavedWorkoutExercises[i];
+			const newExerciseProps = {
+				name,
+				sets,
+				reps,
+				intervals,
+				duration,
+				intensity
+			};
+			const newExercise = await context.prisma.createExercise({
+				...newExerciseProps,
+				workout: { connect: { id: newWorkout.id } }
+			});
+		}
+		return newWorkout;
+	} catch (error) {
+		console.log(error);
+		return new Error("Sorry, there was an internal error.");
+	}
+};
 
 const deleteWorkout = async (root, args, context, info) =>
-    await context.prisma.deleteWorkout({ id: args.id });
-    
+	await context.prisma.deleteWorkout({ id: args.id });
 
-const editWorkout = async(root, args, context, info) =>
-    await context.prisma.updateWorkout({
-        data: {name: args.name},
-        where: {id: args.id}
-    })
+const editWorkout = async (root, args, context, info) =>
+	await context.prisma.updateWorkout({
+		data: { name: args.name },
+		where: { id: args.id }
+	});
 
 module.exports = {
-    addWorkout,
-    deleteWorkout,
-    editWorkout
-}
+	addWorkout,
+	addWorkoutFromSavedWorkout,
+	deleteWorkout,
+	editWorkout
+};
